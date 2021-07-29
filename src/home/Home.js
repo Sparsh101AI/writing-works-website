657d9f4e73061005412912be2465c9d6127afe3f
Sparsh101AI <sparshpatel6165@gmail.com>
2021-07-29 12:47:05 -0600

    added logo

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,20 +3,48 @@ import './Home.css';
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-
+import Navbar from 'react-bootstrap/Navbar'
 
  function Home(){
 
     return(
         
+
         
     <div>
         
  <Container fluid>
+
   <Row>
-    <Col>1 of 1</Col>
-  </Row>
+    <Col>
+    <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo192.png"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      React Bootstrap
+      </Navbar.Brand>
     </Container>
+  </Navbar>
+    
+    </Col>
+  </Row>
+
+
+
+ </Container>
+
+
+
+
+
+
+
 
     <Container fluid>
   <Row>
